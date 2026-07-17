7081c0074b4db8dd86d40b43fc41231ee5180a17
Surjeet14 <su140105@gmail.com>
2026-07-17 20:35:51 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/surjeet/employee_security_api/EmployeeSecurityApiApplication.java
@@ -0,0 +1,13 @@
+package com.surjeet.employee_security_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSecurityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSecurityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surjeet/employee_security_api/EmployeeSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surjeet.employee_security_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
