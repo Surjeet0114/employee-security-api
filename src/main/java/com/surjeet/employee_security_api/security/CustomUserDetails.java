c1b71ff390d9a0da439f3daf75ccfedd80eb94a5
Surjeet14 <su140105@gmail.com>
2026-07-18 01:19:31 +0530

    Add CustomUserDetails adapter

---
+++ src/main/java/com/surjeet/employee_security_api/security/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package com.surjeet.employee_security_api.security;
+
+import com.surjeet.employee_security_api.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
